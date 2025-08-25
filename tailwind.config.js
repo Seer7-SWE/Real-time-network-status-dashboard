c3e145af12f95e0c52f3c78452e1aeaee8ae701c
Seer7-SWE <seeralan006@gmail.com>
2025-08-25 15:06:15 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
-  plugins: []
+  darkMode: 'class', // 👈 important
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
