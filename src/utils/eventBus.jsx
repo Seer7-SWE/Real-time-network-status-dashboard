d2ffc35f670322f6055ffc0945f3a84a1031a801
Seer7-SWE <seeralan006@gmail.com>
2025-08-25 11:04:21 +0530

    Update eventBus.jsx

--- src/utils/eventBus.jsx
+++ src/utils/eventBus.jsx
@@ -3,12 +3,12 @@ import { createContext, useContext, useEffect, useState } from "react";
 const EventContext = createContext();
 
 const regions = [
-  /*{ name: 'Manama', lat: 26.2285, lng: 50.5860 },
+  { name: 'Manama', lat: 26.2285, lng: 50.5860 },
   { name: 'Al Muharraq', lat: 26.2572, lng: 50.6119 },
   { name: 'Riffa', lat: 26.1278, lng: 50.5620 },
   { name: 'Isa Town', lat: 26.1736, lng: 50.5478 },
   { name: 'Sitra', lat: 26.1547, lng: 50.6206 },
-  { name: 'Saar', lat: 26.1970, lng: 50.4820 },*/
+  { name: 'Saar', lat: 26.1970, lng: 50.4820 },
   { name: 'Hamad Town', lat: 26.1152, lng: 50.50694 },
   { name: 'Jidhafs', lat: 26.2186, lng: 50.54778 },
   { name: 'Al Hidd', lat: 26.2455, lng: 50.65417 },
