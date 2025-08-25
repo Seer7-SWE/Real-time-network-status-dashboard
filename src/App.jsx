ca3c7c3b46e39e60cd3fbd5f32bf86e02282b9ba
Seer7-SWE <seeralan006@gmail.com>
2025-08-25 11:24:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ export default function App() {
            <Alerts />
           <Analytics />
         </div>
-        +        <div className="p-4 space-y-4 bg-gray-100 min-h-screen">
+            <div className="p-4 space-y-4 bg-gray-100 min-h-screen">
           {/* Map */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             <div className="col-span-1 md:col-span-2 lg:col-span-2">
