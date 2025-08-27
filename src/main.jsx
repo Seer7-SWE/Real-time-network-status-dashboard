d6aae330392c4c368ad208e4f890c20c1361cf2f
Seer7-SWE <seeralan006@gmail.com>
2025-08-27 21:50:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { EventProvider } from "./utils/eventBus.jsx";
+import { SettingsProvider } from "./utils/settingsContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <EventProvider>
+    <SettingsProvider>
       <App />
-    </EventProvider>
+    </SettingsProvider>
   </React.StrictMode>
 );
