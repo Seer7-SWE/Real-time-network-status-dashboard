f8fb3657aecc4f8902c46f392a4e0387c818a56a
Seer7-SWE <seeralan006@gmail.com>
2025-08-27 22:28:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { SettingsProvider } from "./utils/settingsContext.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { SettingsProvider } from './utils/settingsContext'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SettingsProvider>
       <App />
     </SettingsProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
